b2f22e6aec2f73fcef97fa9654969f75ac7621fc
Admin <ttttt@gmail.com>
2025-05-10 13:08:55 +1000

    больше доработок

--- data/users.py
+++ data/users.py
@@ -17,7 +17,7 @@ class User(SqlAlchemyBase, UserMixin):
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now().today())
+                                     default=datetime.date.today())
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
